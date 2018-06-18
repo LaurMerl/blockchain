ad81132bead75c8c7343aa1bb14e9fb047513c9b
Laura Merlo <l.merlo4@gmail.com>
2018-06-18 18:43:59 +0200

    Added index endpoint and utils library

---
+++ index.js
@@ -0,0 +1,30 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const blockchain = require( './blockchain');
+const block = require( './block');
+
+const Blockchain = new blockchain();
+const Block = new block();
+
+const httpPort = parseInt(process.env.HTTP_PORT) || 3001;
+
+const initHttpServer = (myHttpPort) => {
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => {
+        res.send(Blockchain.getBlockchain());
+    });
+
+    app.post('/mineBlock', (req, res) => {
+        const newBlock = Block.generateNextBlock(req.body.data, Blockchain);
+        res.send(newBlock);
+    });
+
+    app.listen(myHttpPort, () => {
+        console.log('Listening http on port: ' + myHttpPort);
+    });
+};
+
+initHttpServer(httpPort);
---
+++ utils.js
@@ -0,0 +1,39 @@
+// in seconds
+const BLOCK_GENERATION_INTERVAL = 10;
+
+// in blocks
+const DIFFICULTY_ADJUSTMENT_INTERVAL = 10;
+
+const getCurrentTimestamp = () => Math.round(new Date().getTime() / 1000);
+
+const lookupTable = {
+    '0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100',
+    '5': '0101', '6': '0110', '7': '0111', '8': '1000', '9': '1001',
+    'a': '1010', 'b': '1011', 'c': '1100', 'd': '1101',
+    'e': '1110', 'f': '1111'
+};
+
+const getCurrentDate = () => {
+    const today = new Date();
+    return today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear();
+};
+
+const hexToBinary = (s) => {
+    let ret = '';
+    for (let i = 0; i < s.length; i = i + 1) {
+        if (lookupTable[s[i]]) {
+            ret += lookupTable[s[i]];
+        } else {
+            return null;
+        }
+    }
+    return ret;
+};
+
+module.exports = {
+    BLOCK_GENERATION_INTERVAL,
+    DIFFICULTY_ADJUSTMENT_INTERVAL,
+    getCurrentTimestamp,
+    getCurrentDate,
+    hexToBinary
+};
